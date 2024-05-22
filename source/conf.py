d65fb56a1d156698ecaa4867140a66c33ba1505d
zhengpanone <zhengpanone@htomail.com>
2024-05-22 14:38:05 +0800

    upate config

--- source/conf.py
+++ source/conf.py
@@ -4,14 +4,14 @@ import sys
 import sphinx_rtd_theme
 
 # project information
-project = u'PAN\'s BLOG'
+project = u'JAVA BLOG'
 copyright = u'2018, 郑攀'
 version = '1.0'
 release = '1.0.0'
 language = 'zh_CN'
 # gengral configuration
 
-extensions = ['chinese_search', 'sphinx_copybutton']
+extensions = ['chinese_search', 'recommonmark', 'sphinx.ext.autodoc','sphinx_copybutton']
 templates_path = ['_templates']
 source_suffix = ['.rst', '.md']
 source_encoding = 'utf-8'
@@ -25,14 +25,16 @@ html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 html_static_path = ['../build/html/_static']
-htmlhelp_basename = 'Pan\'s Java Blogs'
+htmlhelp_basename = 'Java Blogs'
 
 formats = ["htmlzip", "pdf", "epub"]
 
 latex_elements = {
     'papersize': 'a4paper',
-    'pointsize': '15pt',
-    'classoptions': ',oneside', 'babel': '',
+    'pointsize': '16pt',
+    'classoptions': ',oneside', 
+    'figure_align': 'htbp',
+    'babel': '',
     'inputenc': '',  # 必须
     'utf8extra': '',  # 必须
     # Additional stuff for the LaTeX preamble.
@@ -54,8 +56,8 @@ man_pages = [
 
 
 texinfo_documents = [
-    ('index', 'PansBlog', 'Pan\'s Blog Documentation',
-     u'郑攀', 'PansBlog', 'One line description of project.',
+    ('index', 'PansBlog', '《Java博客》',
+     u'郑攀', 'PansBlog', '《Java博客》',
      'Miscellaneous'),
 ]
 
